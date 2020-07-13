d7f027c56454d8971ed4e0ab4555d3650e1c1419
Alois Napitalai <anapitalai@gmail.com>
2020-07-13 12:42:42 +1000

    Update server.js
    Add access controls

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const server = app.listen(4000, function () {
     console.log('Listening on port 4000')
 })
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
+  next();
+});
+
 
 //socket setup
 const io = socket(server)
