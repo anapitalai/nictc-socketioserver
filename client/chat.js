43a7d0e249796b76cc185c5ab767642a72875b16
Alois Napitalai <anapitalai@gmail.com>
2020-07-15 12:19:12 +1000

    fix error when adding geolocation

--- client/chat.js
+++ client/chat.js
@@ -1,43 +1,52 @@
-const socket=io.connect('https://realtime.nictconsultants.com')
+const socket = io.connect('https://realtime.nictconsultants.com')
 
 
-const name=document.getElementById('name')
-const message=document.getElementById('message')
-const output=document.getElementById('output')
-const send=document.getElementById('send')
-const typing=document.getElementById('typing')
+const name = document.getElementById('name')
+const message = document.getElementById('message')
+const output = document.getElementById('output')
+const send = document.getElementById('send')
+const typing = document.getElementById('typing')
 
 
-navigator.geolocation.getCurrentPosition(post=>{
-    let latitude=post.coords.latitude
-    let longitude=post.coords.longitude
+navigator.geolocation.getCurrentPosition(post => {
+    let latitude = post.coords.latitude
+    let longitude = post.coords.longitude
 })
 
-send.addEventListener('click',function(){
-    socket.emit('chat',{
-        name:name.value,
-        latitude:latitude,
-        longitude:longitude,
-        message:message.value    
+send.addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(post => {
+        const latitude = post.coords.latitude
+        let longitude = post.coords.longitude
+
+        socket.emit('chat', {
+            name: name.value,
+            latitude: latitude,
+            longitude: longitude,
+            message: message.value
+        })
+
+
+        message.value = ""
     })
 
-message.value=""
+
+
 })
 
-socket.on('chat',function(data){
-    typing.innerHTML=""
-    output.innerHTML += '<p><strong>' + data.name + ':</strong>'+ data.message + '</p>'
+socket.on('chat', function (data) {
+    typing.innerHTML = ""
+    output.innerHTML += '<p><strong>' + data.name + ':</strong>' + data.message + '</p>'
 
 })
 
 
 //typing
-message.addEventListener('keypress',function(){
-    socket.emit('typing',name.value)
+message.addEventListener('keypress', function () {
+    socket.emit('typing', name.value)
 })
 
-socket.on('typing',function(data){
-    typing.innerHTML = '<p><em>' + data.name + ' is typing a message...' + ' at the coordinates' + latitude+':'+ longitude + ':</em></p>'
+socket.on('typing', function (data) {
+    typing.innerHTML = '<p><em>' + data.name + ' is typing a message...' + ' at the coordinates' + latitude + ':' + longitude + ':</em></p>'
 
 })
 
